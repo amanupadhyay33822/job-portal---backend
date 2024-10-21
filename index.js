7f9f0346e48904077a7f0eec8d2f8c631fa1c91e
Aman Upadhyay <104005869+amanupadhyay33822@users.noreply.github.com>
2024-10-21 14:11:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:['http://localhost:5173','https://job-portal-frontend-nu-dun.vercel.app'],
+    origin:['*'],
     credentials:true
 }
 
